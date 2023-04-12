9ca8435b61a36a71e2f3f64427d7196ab5773232
sakura0711 <testuseroozx@gmail.com>
2023-04-12 16:26:06 +0800

    引入 bootstrap 5

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/scss/bootstrap.scss";
+// import "bootstrap/dist/js/bootstrap.js";
+import * as bootstrap from 'bootstrap'
+import "../src/scss/index.scss";
 
 const app = createApp(App)
 
