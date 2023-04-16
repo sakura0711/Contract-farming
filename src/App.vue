521c6dd78ff31b62dbe6f990474b82bbe22ade95
sakura0711 <testuseroozx@gmail.com>
2023-04-17 02:05:38 +0800

    delete  non-essential code

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import NavBar from '@/components/NavBar.vue'
+import NavBar from '@/components/global/NavBar.vue'
 
 </script>
 
 <script>
-
-// ? 刷新頁面 / 導向同頁面
-export default {
-  // 可以被子組件繼承 ，跟 inject 一起使用
-  provide() {
-    return { reload: this.reload }
-  },
-
-  data() {
-    return {
-      isRouterAlive: true,
-    }
-  },
-  methods: {
-    reload() {
-      console.log(`reload`);
-      this.isRouterAlive = false;
-      this.$nextTick(() => {
-        this.isRouterAlive = true;
-      })
-    }
-  },
-}
-
 </script>
 
 <template>
